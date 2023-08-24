20af8e645c8b35fc85571d531644936a3c560d19
Mich2504s <142708943+Mich2504s@users.noreply.github.com>
2023-08-23 21:13:09 -0600

    Barra de navegacion 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navegacion/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Combos from './components/paginas/Combos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+<Router>
+  <Navbar/>
+</Router>
+
+   </div>
+
+
   );
 }
 
